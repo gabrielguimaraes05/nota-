b22f2d8bf8ace5cc3ca2842c6f3e52d768053029
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2020-04-06 20:16:08 -0300

    Adicionando método compareSync

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -26,7 +26,7 @@ class User extends Model {
   }
 
   checkPassword(password) {
-    return bcrypt.compare(password, this.password_hash);
+    return bcrypt.compareSync(password, this.password_hash);
   }
 }
 
