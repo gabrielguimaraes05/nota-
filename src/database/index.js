309f82b2ff9fdc722bed70dd10092f371d0e45ee
gabrielguimaraes05 <gabriel.prguimaraes@gmail.com>
2020-05-02 16:49:28 -0300

    correção database/index.js

--- src/database/index.js
+++ src/database/index.js
@@ -3,15 +3,12 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
-
-import databaseConfig from '../config/database';
-
-const models = [User, File];
 import Order from '../app/models/Order';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Order];
+
+const models = [User, File, Order];
 
 class Database {
   constructor() {
